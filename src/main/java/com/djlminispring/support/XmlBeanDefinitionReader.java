5300d78309829bd10814b8b4cb024327c63f62aa
djl <djl@cis.com>
2019-04-25 17:40:00 +0800

    context

--- src/main/java/com/djlminispring/support/XmlBeanDefinitionReader.java
+++ src/main/java/com/djlminispring/support/XmlBeanDefinitionReader.java
@@ -1,10 +1,16 @@
 package com.djlminispring.support;
 
 import com.djlminispring.io.Resource;
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @Author: djl
@@ -19,6 +25,26 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
 
     @Override
     public void loadBeanDefinitions(Resource resource) throws IOException, ParserConfigurationException, SAXException, ClassNotFoundException {
+        InputStream inputStream = null;
+        try {
+            inputStream = resource.getInputStream();
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+            Document document = builder.parse(inputStream);
+            registerBeanDefinitions(document,resource);
+        } catch (Exception e) {
+            
+        } finally {
+            
+        }
+    }
 
+    private void registerBeanDefinitions(Document document, Resource resource) throws ClassNotFoundException{
+        Element documentElement = document.getDocumentElement();
+        doRegisterBeanDefinitions(documentElement);
+    }
+
+    private void doRegisterBeanDefinitions(Element documentElement) throws ClassNotFoundException{
+        
     }
 }
